d8a6ab53c3209b791822f1fd2a0c1794b8da62f1
Ray Epps <rayepps@Rays-MacBook-Air.local>
2023-09-23 22:02:48 -0600

    strip down

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,16 +30,16 @@ export default function Home() {
             <div>
               <span className="font-bold text-blue-600">Exobase</span>
               <p className="text-slate-900 text-4xl tracking-tight font-extrabold sm:text-5xl max-w-[20vw]">
-                Rapidly Build APIs that are out of this world.
+                Making multi cloud infrastructure possible
               </p>
-              <div className="mt-8 p-4 border border-slate-200 rounded-lg inline-block">
+              {/* <div className="mt-8 p-4 border border-slate-200 rounded-lg inline-block">
                 <span className="text-slate-700 font-semibold">yarn add @exobase/core</span>
-              </div>
+              </div> */}
             </div>
             <div className="grow flex items-center justify-end">
-              <span className="block text-slate-900 hover:text-slate-800">Docs</span>
+              {/* <span className="block text-slate-900 hover:text-slate-800">Docs</span>
               <span className="ml-6 block text-slate-900 hover:text-slate-800">Blog</span>
-              <span className="ml-6 block text-slate-900 hover:text-slate-800">Community</span>
+              <span className="ml-6 block text-slate-900 hover:text-slate-800">Community</span> */}
               <a
                 href="https://github.com/exobase-inc"
                 className="ml-6 block text-slate-900 hover:text-slate-800"
@@ -58,57 +58,13 @@ export default function Home() {
           </div>
           <div className="absolute bottom-16 left-0 pt-16 px-16 w-full flex items-start">
             <div>
-              <span className="text-slate-900 text-xl italic font-medium">Better than best practices</span>
+              <span className="text-slate-900 text-xl italic font-medium">A project in progress...</span>
             </div>
           </div>
           <Logo className="w-[20vw] fill-slate-900" />
         </div>
       </div>
 
-      {/* Best Practices Quote */}
-      <section className="text-center px-8 mt-60">
-        <h2 className="text-slate-900 text-4xl tracking-tight font-extrabold sm:text-5xl">
-          “best practices”? We can do better.
-        </h2>
-        <figure>
-          <blockquote>
-            <p className="mt-6 max-w-3xl mx-auto text-lg">
-              Over my 10 years of building I've had to solve the same problems 
-              on every project. I spent 4 years designing this solution so we 
-              can all do better than best practices. Try to keep an open mind.
-            </p>
-          </blockquote>
-          <figcaption className="mt-6 flex items-center justify-center space-x-4 text-left">
-            <img
-              src={require('@/img/ray.jpg').default}
-              alt=""
-              className="w-14 h-14 rounded-full"
-              loading="lazy"
-            />
-            <div>
-              <div className="text-slate-900 font-semibold">Ray Epps</div>
-              <div className="mt-0.5 text-sm leading-6">Creator of Exobase</div>
-            </div>
-          </figcaption>
-        </figure>
-      </section>
-
-      {/* The Design Pattern */}
-      <section className="my-80">
-        <TheDesignPattern />
-      </section>
-
-      {/* The Library */}
-      <section className="my-80">
-        <TheLibrary />
-      </section>
-      
-      {/* The Architecture */}
-      <section className="my-80">
-        <TheArchitecture />
-      </section>
-
-      <Footer />
     </>
   )
 }
